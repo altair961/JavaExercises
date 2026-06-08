9b9d8b16c1371fd188d7dbb80cafac337f472d8d
altair961 <altair89@list.ru>
2026-06-08 18:59:34 +0300

    Implement 9th exercise from self test

---
+++ SchildtJavaBeginnerGuide/Ch1_st9/src/Moon.java
@@ -0,0 +1,9 @@
+//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
+// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
+public class Moon {
+    public static void main(String[] args) {
+        var myMassKgOnEarth = 89;
+        var myMassKgOnMoon = 89 * 0.17;
+        System.out.println(myMassKgOnEarth + " kg earthweight is equivalent to moonweight " + myMassKgOnMoon + " kg.");
+    }
+}
