ac7b98cb19485922a1c0999709c66673c0e46fb2
altair961 <altair89@list.ru>
2026-06-10 15:13:41 +0300

    Implement Ch1Ex1, Ch1Ex1-2

---
+++ UllenboomJavaProgrammingExercisesVol1/Ch1Ex1-2/src/Application.java
@@ -0,0 +1,9 @@
+public class Application {
+    public static void main(String[] args) {
+        System.out.println("Aye Captain!");
+
+        for(char i = 913; i < 1023; i++) {
+            System.out.println("heigh ho! " + i);
+        }
+    }
+}
