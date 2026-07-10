89adde4e5d4f0683d0efdd6d87ebf85c35aa6ea9
altair961 <altair89@list.ru>
2026-07-10 18:00:15 +0300

    Implement ch2ex1

---
+++ UllenboomVol1/ch2ex1/src/main/java/com/altair961/ch2ex1/SvgCircle1.java
@@ -0,0 +1,18 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.altair961.ch2ex1;
+
+/**
+ *
+ * @author ubuntu
+ */
+public class SvgCircle1 {
+
+    public static void main(String[] args) {
+        System.out.println("<svg height=\"400\" width=\"1000\">");
+        System.out.println("\t<circle cx=\"100\" cy=\"100\" r=\"50\" />");
+        System.out.println("</svg>");
+    }
+}
