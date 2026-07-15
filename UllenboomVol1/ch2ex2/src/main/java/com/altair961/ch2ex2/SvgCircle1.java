b5199ad7b074f68edb43d2080f7adb0d5b59aed8
altair961 <altair89@list.ru>
2026-07-16 01:44:28 +0300

    Implement ch2_ex2

--- UllenboomVol1/ch2ex2/src/main/java/com/altair961/ch2ex2/SvgCircle1.java
+++ UllenboomVol1/ch2ex2/src/main/java/com/altair961/ch2ex2/SvgCircle1.java
@@ -2,7 +2,9 @@
  * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
  */
 
-package com.altair961.ch2ex1;
+package com.altair961.ch2ex2;
+
+import java.math.BigDecimal;
 
 /**
  *
@@ -10,8 +12,12 @@ package com.altair961.ch2ex1;
  */
 public class SvgCircle1 {
     public static void main(String[] args) {
+        int x = 100;
+        int y = 110;
+        double r = 20.5;
         System.out.println("<svg height=\"400\" width=\"1000\">");
-        System.out.println("\t<circle cx=\"100\" cy=\"100\" r=\"50\" />");
+        System.out.println("\t<circle cx=\"" + x + "\" cy=\"" + y + "\" r=\"" + r + "\" />");
         System.out.println("</svg>");
     }
 }
+
