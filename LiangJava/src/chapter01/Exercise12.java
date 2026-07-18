41663fcdd42caa65b6b489a8928165ab02ae7b86
altair961 <altair89@list.ru>
2026-07-18 13:44:43 +0300

    Implement exercise 12

---
+++ LiangJava/src/chapter01/Exercise12.java
@@ -0,0 +1,13 @@
+package chapter01;
+
+public class Exercise12 {
+
+	public static void main(String[] args) {
+		double hours = 1 + (40.0 + 35.0 / 60) / 60;
+		double km = 24 * 1.6;
+		double kmPerHour = km / hours; 
+		System.out.println("km/h: " + kmPerHour);
+		
+	}
+
+}
