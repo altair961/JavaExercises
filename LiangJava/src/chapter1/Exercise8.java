ca323be1b60b56083d686a987b0a786d64920f2d
altair961 <altair89@list.ru>
2026-07-18 12:25:35 +0300

    Implement exercise 8

---
+++ LiangJava/src/chapter1/Exercise8.java
@@ -0,0 +1,11 @@
+package chapter1;
+
+public class Exercise8 {
+
+	public static void main(String[] args) {
+		System.out.println("Perimeter: " + 2 * 6.5 * 3.14);
+		System.out.println("Area: " + 6.5 * 6.5 * 3.14);
+
+	}
+
+}
