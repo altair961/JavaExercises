fa0f7cd7eb95bb3196787fead07324aedff3cca8
altair961 <altair89@list.ru>
2026-07-18 11:49:12 +0300

    Implement exercise 3

---
+++ LiangJava/src/chapter1/Exercise3.java
@@ -0,0 +1,14 @@
+package chapter1;
+
+public class Exercise3 {
+
+	public static void main(String[] args) {
+		System.out.println("   J");
+		System.out.println("J aaa v vaaa");
+		System.out.println("J J aa vv a a");
+		System.out.println("J aaaa");
+		System.out.println("V aaaa");
+
+	}
+
+}
